2d749d6fe0e4dd6508540c65c817390a11872d2c
Platform Claims <platformclaims@Platforms-MacBook-Pro.local>
2025-05-13 12:42:42 +0300

    init

---
+++ ios/DynamicIslandBridge.m
@@ -0,0 +1,14 @@
+//
+//  DynamicIslandBridge.m
+//  trips
+//
+//  Created by Platform Claims on 13/05/2025.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(DynamicIslandModule, NSObject)
+
+RCT_EXTERN_METHOD(testFunc)
+
+@end
---
+++ ios/DynamicIslandModule.swift
@@ -0,0 +1,15 @@
+//
+//  DynamicIslandModule.swift
+//  trips
+//
+//  Created by Platform Claims on 13/05/2025.
+//
+
+@objc(DynamicIslandModule)
+class DynamicIslandModule: NSObject {
+  
+  @objc(testFunc)
+  func testFunc() {
+    print("Hi this is from swift module!")
+  }
+}
